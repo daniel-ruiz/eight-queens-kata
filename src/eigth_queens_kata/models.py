4a5ac4fa5914fb5737a8d91fb936ff3289d67a00
Daniel Ruiz <danruicar@gmail.com>
2022-10-21 20:16:12 +0200

    Make queen capture other queens diagonally

--- src/eigth_queens_kata/models.py
+++ src/eigth_queens_kata/models.py
@@ -18,6 +18,7 @@ class Queen:
         return (
             self._is_other_queen_in_same_row(other_queen)
             or self._is_other_queen_in_same_file(other_queen)
+            or self._is_other_queen_in_a_diagonal(other_queen)
         )
 
     def _is_other_queen_in_same_file(self, other_queen):
@@ -25,3 +26,6 @@ class Queen:
 
     def _is_other_queen_in_same_row(self, other_queen: "Queen") -> bool:
         return self.row == other_queen.row
+
+    def _is_other_queen_in_a_diagonal(self, other_queen: "Queen") -> bool:
+        return self.file - other_queen.file == self.row - other_queen.row
--- src/tests/test_queen.py
+++ src/tests/test_queen.py
@@ -10,3 +10,7 @@ class TestQueen:
     def test_queen_can_capture_queens_of_the_same_file(self):
         assert Queen("f5").can_capture(Queen("f1"))
         assert Queen("f1").can_capture(Queen("f5"))
+
+    def test_queen_can_capture_queens_in_one_diagonal(self):
+        assert Queen("c4").can_capture(Queen("f7"))
+        assert Queen("f7").can_capture(Queen("c4"))
