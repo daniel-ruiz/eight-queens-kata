5d92fa5622f505db8d8f0ee04e9bfaee6c6d3e13
Daniel Ruiz <danruicar@gmail.com>
2022-10-21 20:10:16 +0200

    Improve readability in tests

--- src/tests/test_queen.py
+++ src/tests/test_queen.py
@@ -4,8 +4,8 @@ from eigth_queens_kata.models import Queen
 class TestQueen:
 
     def test_queen_can_capture_queens_of_the_same_row(self):
-        assert Queen("a3").can_capture(Queen("d3")) is True
-        assert Queen("d3").can_capture(Queen("a3")) is True
+        assert Queen("a3").can_capture(Queen("d3"))
+        assert Queen("d3").can_capture(Queen("a3"))
 
     def test_queen_can_capture_queens_of_the_same_file(self):
         assert Queen("f5").can_capture(Queen("f1"))
