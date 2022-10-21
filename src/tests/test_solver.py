019795cab4972cb9f3cc54d859737bdfcdfec857
Daniel Ruiz <danruicar@gmail.com>
2022-10-21 20:41:52 +0200

    Add failing test to build the solver

---
+++ src/tests/test_solver.py
@@ -0,0 +1,15 @@
+class TestSolver:
+
+    def test_returns_eight_queens_that_cannot_be_captured_between_themselves(self):
+        solver = EightQueensSolver()
+
+        solution = solver.solve()
+
+        assert len(solution) == 8
+        
+        for i in range(8):
+            for j in range(i + 1, 8):
+                queen_i = solution[i]
+                queen_j = solution[j]
+    
+                assert not queen_i.can_capture(queen_j)
